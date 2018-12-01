0c99c279b09d707dc9bb80b79b45a88bf065763b
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-12-01 23:45:03 +0800

    build test

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,3 @@
-const canvas = document.getElementById('cvs')
-const ctx = canvas.getContext('2d')
-
 const app = new Vue({
   el: '#app',
   data: {
@@ -18,6 +15,9 @@ const app = new Vue({
     fileList: [],
     imgLoading: false,
     cssStr: '',
+    cssAttr: {
+      unit: 'px',
+    },
     imgStr: '',
     btnText: '开始生成',
     btnLoading: false
@@ -44,7 +44,26 @@ const app = new Vue({
     fileRemove: function(file, fileList) {
       this.fileList = fileList
     },
+    dealCss: function (row, col) {
+      let step = (100 / this.fileList.length).toFixed(2)
+      let frames = ''
+      for (let i = 0; i < this.fileList.length; i++) {
+        if (i === 0) {
+          frames += `  0% { background-position: 0 0; }`
+        } else if (i === this.fileList.length - 1) {
+          frames += `\n  100% { background-position: 0 0; }`
+        } else {
+          frames += `\n  ${(step * i).toFixed(2)}% { background-position: 0 0; }`
+        }
+      }
+      console.log(frames)
+    },
     startDealing: function(tmp) {
+      let canvas = document.getElementById('cvs')
+      if (tmp === 0) {
+        canvas.height = canvas.height
+      }
+      let ctx = canvas.getContext('2d')
       let self = this
       let myTmp = tmp
       let img = new Image()
@@ -53,29 +72,40 @@ const app = new Vue({
       this.btnLoading = true
       img.onload = function() {
         // 绘制对应的图形
-        console.log(self.fileList[myTmp].name)
+        // console.log(self.fileList[myTmp].name)
         let row = 1
         let col = 1
         switch (self.sizeType) {
           case '1':
-          col = +self.canvasData.width / +self.frameData.prewidth
-          row = +self.canvasData.height / +self.frameData.preheight
-          break
+            col = +self.canvasData.width / +self.frameData.prewidth
+            row = +self.canvasData.height / +self.frameData.preheight
+            break
           case '2':
-          col = +self.frameData.col
-          row = +self.frameData.row
-          break
+            col = +self.frameData.col
+            row = +self.frameData.row
+            break
         }
-        ctx.drawImage(img, )
+        if (myTmp === 0) {
+          self.dealCss(row, col)
+        }
+        // console.log((myTmp % col) * +self.frameData.prewidth)
+        // console.log(Math.floor(myTmp / col) * +self.frameData.preheight)
+        ctx.drawImage(
+          img,
+          (myTmp % col) * +self.frameData.prewidth,
+          Math.floor(myTmp / col) * +self.frameData.preheight
+        )
         myTmp++
         if (self.fileList.length > myTmp) {
           setTimeout(function() {
             self.startDealing(myTmp)
           }, 1000)
         } else {
+          // console.log(canvas.toDataURL('image/png'))
+          self.imgStr = canvas.toDataURL('image/png')
           self.btnText = '开始生成'
           self.btnLoading = false
-          self.$message.success('图片生成完成');
+          self.$message.success('图片生成完成')
         }
       }
       reader.onload = function(e) {
