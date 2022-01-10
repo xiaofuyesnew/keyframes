2db450e8ddae6c4005cd90c00a7cfc2a382d55c8
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-01-11 01:03:28 +0800

    update

--- src/components/DragList.jsx
+++ src/components/DragList.jsx
@@ -1,14 +1,41 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { Context } from '../store/index'
 
-const DragList = ({ list = [] }) => {
-  return (
-    <div>
-      {list.map((item, index) => (
-        <div className=""></div>
-      ))}
-    </div>
-  )
+import { Empty, Spin } from 'antd'
+
+const DragList = ({ list = [], loading = false }) => {
+  const onDragStart = (e) => {
+    console.log(e)
+  }
+
+  const onDragEnd = (e) => {
+    console.log(e)
+  }
+
+  const onDragOver = (e) => {
+    console.log(e)
+  }
+
+  const onDrop = (e) => {
+    console.log(e)
+  }
+
+  if (list.length) {
+    return (
+      <ul className="">
+        <div
+          className="w-[100px] h-[100px] bg-slate-600"
+          draggable
+          onDragStart={onDragStart}
+        ></div>
+        {list.map((item, index) => (
+          <div className="test" draggable onDragstart={onDragStart}></div>
+        ))}
+      </ul>
+    )
+  } else {
+    return <Empty className="!mt-[100px]" description={false} />
+  }
 }
 
 export default DragList
--- src/components/ImgList.jsx
+++ src/components/ImgList.jsx
@@ -4,10 +4,24 @@ import { CloudUploadOutlined } from '@ant-design/icons'
 import DragList from './DragList'
 
 const ImgList = () => {
+  const handleChange = (e) => {
+    console.log(e.currentTarget.files)
+  }
+
   return (
     <div className="options">
-      <Button type="primary" className="!flex items-center" icon={<CloudUploadOutlined />}>
-        upload images
+      <Button
+        type="primary"
+        className="!flex items-center relative"
+        icon={<CloudUploadOutlined />}
+      >
+        上传图片
+        <input
+          className="absolute top-0 left-0 w-[100%] h-[100%] opacity-0"
+          type="file"
+          multiple
+          onChange={handleChange}
+        />
       </Button>
       <DragList />
     </div>
